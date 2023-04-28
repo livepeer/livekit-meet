eb018711c2c6c6e190f991bb31ee1361a73e06e3
Max Holland <max@livepeer.org>
2023-04-28 16:48:40 +0100

    Set a max bitrate and resolution

--- pages/custom/index.tsx
+++ pages/custom/index.tsx
@@ -1,5 +1,5 @@
 import { formatChatMessageLinks, LiveKitRoom, VideoConference } from '@livekit/components-react';
-import { LogLevel } from 'livekit-client';
+import { LogLevel, VideoPresets } from 'livekit-client';
 import { useRouter } from 'next/router';
 import { DebugMode } from '../../lib/Debug';
 
@@ -13,10 +13,26 @@ export default function CustomRoomConnection() {
     return <h2>Missing LiveKit token</h2>;
   }
 
+  const roomOptions = {
+    videoCaptureDefaults: {
+      resolution: VideoPresets.h540,
+    },
+    publishDefaults: {
+      videoEncoding: {
+        maxBitrate: 600_000,
+        maxFramerate: 30,
+      },
+      screenShareEncoding: {
+        maxBitrate: 1_000_000,
+        maxFramerate: 30,
+      },
+    }
+  };
+
   return (
     <main data-lk-theme="default">
       {liveKitUrl && (
-        <LiveKitRoom token={token} serverUrl={liveKitUrl} audio={true} video={true}>
+        <LiveKitRoom token={token} serverUrl={liveKitUrl} audio={true} video={true} options={roomOptions}>
           <VideoConference chatMessageFormatter={formatChatMessageLinks} />
           <DebugMode logLevel={LogLevel.info} />
         </LiveKitRoom>
