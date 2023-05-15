1a0a6b582e5c4b602974405bb6ca1cbadaff3fa8
Max Holland <max@livepeer.org>
2023-05-15 16:39:49 +0100

    add debug logging

--- pages/custom/index.tsx
+++ pages/custom/index.tsx
@@ -1,19 +1,14 @@
 import { formatChatMessageLinks, LiveKitRoom, VideoConference } from '@livekit/components-react';
-import { LogLevel, VideoPresets } from 'livekit-client';
+import { LogLevel, VideoPresets, Room, RoomEvent } from 'livekit-client';
 import { useRouter } from 'next/router';
 import { DebugMode } from '../../lib/Debug';
+import React from 'react';
 
 export default function CustomRoomConnection() {
   const router = useRouter();
   const { liveKitUrl, token } = router.query;
-  if (typeof liveKitUrl !== 'string') {
-    return <h2>Missing URL</h2>;
-  }
-  if (typeof token !== 'string') {
-    return <h2>Missing token</h2>;
-  }
 
-  const roomOptions = {
+  const myRoom = new Room({
     videoCaptureDefaults: {
       resolution: VideoPresets.h540,
     },
@@ -26,15 +21,50 @@ export default function CustomRoomConnection() {
         maxBitrate: 1_000_000,
         maxFramerate: 30,
       },
-    }
-  };
+    },
+  });
+
+  const logTrackInfo = function() {
+    console.log('participant count: ' + myRoom.participants.size)
+    myRoom.participants.forEach((p) => {
+      p.audioTracks.forEach((t) => {
+        console.log('participant tracks', 'participant', p.name, 'track', t)
+      });
+    });
+  }
+
+  const onPlaybackStatusChanged = React.useCallback((status) => {
+    logTrackInfo()
+    console.log('onPlaybackStatusChanged. status: ' + status)
+  }, []);
+  const onTrackSubscribed = React.useCallback((track, publication, participant) => {
+    logTrackInfo()
+    console.log('onTrackSubscribed.', 'track', track, 'participant', participant)
+  }, [])
+  const onTrackUnSubscribed = React.useCallback((track, publication, participant) => {
+    logTrackInfo()
+    console.log('onTrackUnSubscribed.', 'track', track, 'participant', participant)
+  }, [])
+
+  if (typeof liveKitUrl !== 'string') {
+    return <h2>Missing URL</h2>;
+  }
+  if (typeof token !== 'string') {
+    return <h2>Missing token</h2>;
+  }
+
+  myRoom.connect(liveKitUrl, token)
+  myRoom
+      .on(RoomEvent.AudioPlaybackStatusChanged, onPlaybackStatusChanged)
+      .on(RoomEvent.TrackSubscribed, onTrackSubscribed)
+      .on(RoomEvent.TrackUnsubscribed, onTrackUnSubscribed)
 
   return (
     <main data-lk-theme="default">
       {liveKitUrl && (
-        <LiveKitRoom token={token} serverUrl={liveKitUrl} audio={true} video={true} options={roomOptions}>
+        <LiveKitRoom room={myRoom} audio={true} video={true}>
           <VideoConference chatMessageFormatter={formatChatMessageLinks} />
-          <DebugMode logLevel={LogLevel.info} />
+          <DebugMode logLevel={LogLevel.debug} />
         </LiveKitRoom>
       )}
     </main>
